ebbc09c42c3d3639b20ff9861d78f3c240b239ee
Bobby Rath <rar.rath@gmail.com>
2018-05-13 23:16:27 -0500

    Added Ado.Net to ProductController

--- BoardGameStore/Controllers/ProductController.cs
+++ BoardGameStore/Controllers/ProductController.cs
@@ -4,37 +4,80 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BoardGameStore.Models;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace BoardGameStore.Controllers
 {
     public class ProductController : Controller
     {
-        private List<Product> _products;
+        //private List<Product> _products;
+        private string _ProductConnectionString = null;
 
-        public ProductController()
+
+        public ProductController(IConfiguration config)
         {
-            _products = new List<Product>();
-            _products.Add(new Product
-            {
-                ID = 1,
-                Name = "Agricola",
-                Description = "Eurostyle farm building game",
-                Price = 49.99m,
-                Image = "/images/Agricola-cards-in-play.jpg",
-                Category = "Worker Placement"
-            });
-            _products.Add(new Product
-            {
-                ID = 2,
-                Name = "Mysterium",
-                Description = "Interpretive card image clue giving game",
-                Price = 39.99m,
-                Image = "/images/mysterium.jpg",
-                Category = "Party"
-            });
+            _ProductConnectionString = config.GetConnectionString("Product");
         }
+
+        //_products = new List<Product>();
+        //Image = "/images/Agricola-cards-in-play.jpg",
+        //Image = "/images/mysterium.jpg",
+
         public IActionResult Index()
         {
+            List<Product> _products = new List<Product>();
+            using (SqlConnection connection = new SqlConnection(_ProductConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "spGetAllProducts";
+                    command.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        int idColumn = reader.GetOrdinal("ID");
+                        int nameColumn = reader.GetOrdinal("ProductName");
+                        int descriptionColumn = reader.GetOrdinal("Description");
+                        int imageColumn = reader.GetOrdinal("Image");
+                        while (reader.Read())
+                        {
+                            int productModelID = reader.GetInt32(idColumn);
+                            string name = reader.GetString(nameColumn);
+                            string description = reader.GetString(descriptionColumn);
+                            string image = reader.GetString(imageColumn);
+                            _products.Add(new Product
+                            {
+                                ID = productModelID,
+                                Description = description,
+                                Name = name,
+                                Image = image
+                            });
+                        }
+                    }
+                }
+
+                //foreach (var product in _products)
+                //{
+                //    using (SqlCommand imageCommand = connection.CreateCommand())
+                //    {
+                //        imageCommand.CommandText = "spGetProductImages";
+                //        imageCommand.CommandType = CommandType.StoredProcedure;
+                //        imageCommand.Parameters.AddWithValue("@productModelID", product.ID);
+                //        using (SqlDataReader reader2 = imageCommand.ExecuteReader())
+                //        {
+                //            while (reader2.Read())
+                //            {
+                //                byte[] imageBytes = (byte[])reader2[0];
+                //                product.Image = "data:image/jpeg;base64, " + Convert.ToBase64String(imageBytes);
+                //                break;
+                //            }
+                //        }
+                //    }
+                //}
+            }
+
             return View(_products);
         }
 
@@ -42,8 +85,7 @@ namespace BoardGameStore.Controllers
         {
             if (id.HasValue)
             {
-                Product p = _products.Single(x => x.ID == id.Value);
-                return View(p);
+                
             }
             return NotFound();
         }
--- BoardGameStore/Startup.cs
+++ BoardGameStore/Startup.cs
@@ -18,12 +18,14 @@ namespace BoardGameStore
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
+        
+public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.GetConnectionString("Product");
+
             services.AddDbContext<IdentityDbContext>(opt => opt.UseInMemoryDatabase("Identities"));
 
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
