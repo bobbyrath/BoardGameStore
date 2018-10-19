4c12ec2998d2ad124838f19528656497b70500cd
Bobby Rath <rar.rath@gmail.com>
2018-10-18 20:57:14 -0500

    Fixed search functionality

--- BoardGameStore/DbInitializer.cs
+++ BoardGameStore/DbInitializer.cs
@@ -13,14 +13,29 @@ namespace BoardGameStore
 
             if (db.Products.Count() == 0)
             {
-                db.Products.Add(new Product
-                {
-                    Description = "Farm game",
-                    Name = "Agricola",
-                    Price = 49.99m,
-                    Image = "/images/agricola.jpg"
-                });
-
+                db.Products.AddRange
+                (
+                    new Product { 
+                        Description = "Farm game",
+                        Name = "Agricola",
+                        Price = 49.99m,
+                        Image = "/images/agricola.jpg"
+                    },
+                    new Product
+                    {
+                        Description = "Party game",
+                        Name = "Mysterium",
+                        Price = 39.99m,
+                        Image = "/images/mysterium.jpg"
+                    },
+                    new Product
+                    {
+                        Description = "Co-op game",
+                        Name = "Betrayal At House On The Hill",
+                        Price = 49.99m,
+                        Image = "/images/betrayal.jpg"
+                    }
+                );
                 db.SaveChanges();
             }
         }
